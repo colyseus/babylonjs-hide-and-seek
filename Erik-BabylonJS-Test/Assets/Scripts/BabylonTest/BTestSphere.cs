81da6361a624020264b1d00ecb3c457474328964
Erik Davis <erikd@lucidsight.com>
2022-08-02 09:40:50 -0700

    Reorganized scripts. Refreshed BTestSphere script for a clean start debugging

--- Erik-BabylonJS-Test/Assets/Scripts/BabylonTest/BTestSphere.cs
+++ Erik-BabylonJS-Test/Assets/Scripts/BabylonTest/BTestSphere.cs
@@ -13,15 +13,11 @@ public class BTestSphere : EditorScriptComponent
     // Example: [Tooltip("Example hello world property")]
     // Example: [Auto] public string helloWorld = "Hello World";
 
-    public float forceMultiplier = 1.0f;
-
 	/* [Serializable, HideInInspector] public string exportProperty = null; */
     public override void OnUpdateProperties(Transform transform, SceneExporterTool exporter)
     {
         // Example: this.helloWorld = "Update Hello World";
-        Debug.Log($"Test Sphere - Update Properties");
     }
-
 }
 
 // Optional Script Component Custom Editor Class
